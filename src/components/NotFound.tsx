140813f25684121b6ba39bb80fbbc9ee2442ef0a
oOBobbyOo <944831575@qq.com>
2025-11-13 10:37:57 +0800

    refactor: change css

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -2,20 +2,20 @@ import { Link } from '@tanstack/react-router'
 
 export function NotFound({ children }: { children?: any }) {
   return (
-    <div className="space-y-2 p-2">
+    <div className="flex flex-col justify-center items-center space-y-4 p-2 py-20">
       <div className="text-gray-600 dark:text-gray-400">
         {children || <p>The page you are looking for does not exist.</p>}
       </div>
       <p className="flex items-center gap-2 flex-wrap">
         <button
           onClick={() => window.history.back()}
-          className="bg-emerald-500 text-white px-2 py-1 rounded-sm uppercase font-black text-sm"
+          className="bg-emerald-500 text-white px-2 py-1 rounded-sm uppercase font-bold text-sm"
         >
           Go back
         </button>
         <Link
           to="/"
-          className="bg-cyan-600 text-white px-2 py-1 rounded-sm uppercase font-black text-sm"
+          className="bg-cyan-600 text-white px-2 py-1 rounded-sm uppercase font-bold text-sm"
         >
           Start Over
         </Link>
