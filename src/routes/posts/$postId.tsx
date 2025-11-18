57009e19ab245231a4e58f53d6fe4e4aafda4352
oOBobbyOo <944831575@qq.com>
2025-11-18 12:05:04 +0800

    refactor: change

--- src/routes/posts/$postId.tsx
+++ src/routes/posts/$postId.tsx
@@ -1,9 +1,46 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { useSuspenseQuery } from '@tanstack/react-query'
+import { ErrorComponent, Link, createFileRoute } from '@tanstack/react-router'
+import type { ErrorComponentProps } from '@tanstack/react-router'
+import { NotFound } from '@/components/NotFound'
+import { postQueryOptions } from '@/utils/posts'
 
 export const Route = createFileRoute('/posts/$postId')({
+  loader: async ({ params: { postId }, context }) => {
+    const data = await context.queryClient.ensureQueryData(
+      postQueryOptions(postId),
+    )
+
+    return {
+      title: data.title,
+    }
+  },
+  head: ({ loaderData }) => ({
+    meta: loaderData ? [{ title: loaderData.title }] : undefined,
+  }),
+  errorComponent: PostErrorComponent,
+  notFoundComponent: () => {
+    return <NotFound>Post not found</NotFound>
+  },
   component: RouteComponent,
 })
 
+function PostErrorComponent({ error }: ErrorComponentProps) {
+  return <ErrorComponent error={error} />
+}
+
 function RouteComponent() {
-  return <div>Hello "/posts/$postId"!</div>
+  const { postId } = Route.useParams()
+  const postQuery = useSuspenseQuery(postQueryOptions(postId))
+
+  return (
+    <div className="space-y-2">
+      <h4 className="text-xl font-bold underline">{postQuery.data.title}</h4>
+      <div className="text-sm">{postQuery.data.body}</div>
+      <div>
+        <Link className="btn text-sm m-3" to="/posts">
+          Back
+        </Link>
+      </div>
+    </div>
+  )
 }
