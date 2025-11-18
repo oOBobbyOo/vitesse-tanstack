cfb45bcd5a85cbdec38f4d0724f2f6a3130ecb9e
oOBobbyOo <944831575@qq.com>
2025-11-18 11:52:51 +0800

    refactor: change posts

--- src/routes/posts/index.tsx
+++ src/routes/posts/index.tsx
@@ -1,9 +1,40 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { useSuspenseQuery } from '@tanstack/react-query'
+import { Link, createFileRoute } from '@tanstack/react-router'
+import { postsQueryOptions } from '@/utils/posts'
 
 export const Route = createFileRoute('/posts/')({
+  loader: async ({ context }) => {
+    await context.queryClient.ensureQueryData(postsQueryOptions())
+  },
+  head: () => ({
+    meta: [{ title: 'Posts' }],
+  }),
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/posts/"!</div>
+  const postsQuery = useSuspenseQuery(postsQueryOptions())
+
+  return (
+    <div className="p-2 flex justify-center gap-2">
+      <ul className="">
+        {[...postsQuery.data].map((post) => {
+          return (
+            <li key={post.id} className="whitespace-nowrap">
+              <Link
+                to="/posts/$postId"
+                params={{
+                  postId: post.id,
+                }}
+                className="block py-2 border-b border-gray-200 dark:border-gray-700 hover:text-blue-600"
+                activeProps={{ className: 'text-black font-bold' }}
+              >
+                <div>{post.title.substring(0, 20)}</div>
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
 }
